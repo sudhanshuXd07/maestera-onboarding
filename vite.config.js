a7418261b431d4f35c91dbc3ebcd36c67d766c30
Om Gayakwad <omgayakwad4@gmail.com>
2025-09-09 14:56:02 +0530

    Fix Vite config and MIME issue

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: ""   // ✅ ensure assets load properly on Vercel
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    port: 5173
+  }
 })
