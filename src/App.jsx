8286322d6df729a61977d236b1235d339ba828e3
Om Gayakwad <omgayakwad4@gmail.com>
2025-09-09 20:40:17 +0530

    Update header: adjust logo position and size, add right text

--- src/App.jsx
+++ src/App.jsx
@@ -221,7 +221,7 @@ export default function App() {
           <img
             src={logo}
             alt="Maestera Logo"
-            className="h-20 object-contain"
+            className="h-40 object-contain"
           />
         </div>
 
