06a715a93947d658ca26929b247dcf51f740bf6f
Om Gayakwad <omgayakwad4@gmail.com>
2025-09-09 14:50:36 +0530

    Add vercel.json to fix asset 404 issue

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/maestera-onboarding/',
-
+  base: ""   // ✅ ensure assets load properly on Vercel
 })
-
