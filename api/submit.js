6cb00b7fa26aecc1ccb96cf6504170bdf086c12f
Om Gayakwad <omgayakwad4@gmail.com>
2025-09-09 17:14:00 +0530

    Move API route into api/

---
+++ api/submit.js
@@ -0,0 +1,11 @@
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    return res.status(200).json({
+      ok: true,
+      message: "Form submitted successfully",
+      data: req.body,
+    });
+  }
+
+  return res.status(405).json({ message: "Method Not Allowed" });
+}
--- pages/api/submit.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = (req, res) => {
-  if (req.method === "POST") {
-    return res.status(200).json({ ok: true, data: req.body });
-  }
-  return res.status(405).json({ message: "Method Not Allowed" });
-};
