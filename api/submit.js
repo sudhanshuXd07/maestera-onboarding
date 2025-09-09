e1fd05b81b077cc71a790b3fe705c8654155a11c
Om Gayakwad <omgayakwad4@gmail.com>
2025-09-09 17:18:48 +0530

    Move API folder to root and fix vercel.json

--- api/submit.js
+++ api/submit.js
@@ -1,11 +1,6 @@
 export default function handler(req, res) {
   if (req.method === "POST") {
-    return res.status(200).json({
-      ok: true,
-      message: "Form submitted successfully",
-      data: req.body,
-    });
+    return res.status(200).json({ ok: true, data: req.body });
   }
-
   return res.status(405).json({ message: "Method Not Allowed" });
 }
