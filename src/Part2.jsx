444f22cba87f94f19b6ff21b0f0332be2a1ec66f
maestroplatform-dev <maestro.platform@gmail.com>
2025-11-17 00:54:32 +0530

    Update Part2.jsx

--- src/Part2.jsx
+++ src/Part2.jsx
@@ -326,15 +326,7 @@ export default function Part2() {
               </div>
             ))}
 
-            <div className="flex flex-col md:flex-row gap-4">
-              <button
-                type="button"
-                onClick={addRow}
-                className="flex-1 px-6 py-2.5 rounded-xl text-white"
-                style={{ backgroundColor: "#0a0a0a" }}
-              >
-                + Add Another Instrument
-              </button>
+            
 
               <div className="border rounded-xl p-5 mb-6 bg-neutral-50">
                 <label className="flex items-center gap-3 cursor-pointer select-none mb-4">
@@ -365,6 +357,16 @@ export default function Part2() {
                   />
                 </div>
             )}
+
+            <div className="flex flex-col md:flex-row gap-4">
+              <button
+                type="button"
+                onClick={addRow}
+                className="flex-1 px-6 py-2.5 rounded-xl text-white"
+                style={{ backgroundColor: "#0a0a0a" }}
+              >
+                + Add Another Instrument
+              </button>
 
 
               <button
